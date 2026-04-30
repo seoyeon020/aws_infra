7c9df91be10f8cc90cbe49e126173992b753b546
seoyeon020 <ksheee7000@naver.com>
2026-04-30 09:27:19 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "project" {
+  default = "myapp"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "key_pair_name" {
+  description = "EC2 접속용 키페어 이름"
+  type        = string
+}
