d63e37069c5818533c37fd994d0484c692b2dc86
seoyeon020 <ksheee7000@naver.com>
2026-04-30 09:32:29 +0900

    keypair update

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "myapp-terraform-state-20260429"
+    bucket         = "myapp-terraform-state-20260429-lsy"
     key            = "training/lab06/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
